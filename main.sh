95d5dcbe2dea53d77f246f202bbe4a2172779e4f
Tom Buytaert <24655941+tbuytaer@users.noreply.github.com>
2022-12-23 13:02:17 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#find ./py_folder -name *.py -print0 | while read -d $'\0' py_file
-#do
-#    python "$py_file"
-#done
-
-python ./py_folder/ICETTFGasFutures.py
+find ./py_folder -name *.py -print0 | while read -d $'\0' py_file
+do
+    python "$py_file"
+done
